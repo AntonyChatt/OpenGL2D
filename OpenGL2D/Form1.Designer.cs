92f5175173d6a39d7e44ff01972dd88795348543
AntonyChatt <nyanchatgalaxy1988@gmail.com>
2020-04-14 19:07:29 +0700

    3.1415dorskiy polygon

---
+++ OpenGL2D/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace OpenGL2D
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.holst = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.line = new System.Windows.Forms.ToolStripMenuItem();
+            this.fill = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // holst
+            // 
+            this.holst.AccumBits = ((byte)(0));
+            this.holst.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.holst.AutoCheckErrors = false;
+            this.holst.AutoFinish = false;
+            this.holst.AutoMakeCurrent = true;
+            this.holst.AutoSwapBuffers = true;
+            this.holst.BackColor = System.Drawing.Color.Black;
+            this.holst.ColorBits = ((byte)(32));
+            this.holst.DepthBits = ((byte)(16));
+            this.holst.Location = new System.Drawing.Point(-1, 27);
+            this.holst.Name = "holst";
+            this.holst.Size = new System.Drawing.Size(802, 535);
+            this.holst.StencilBits = ((byte)(0));
+            this.holst.TabIndex = 0;
+            this.holst.Paint += new System.Windows.Forms.PaintEventHandler(this.Holst_Paint);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(801, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.line,
+            this.fill});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(45, 20);
+            this.toolStripMenuItem1.Text = "Type";
+            // 
+            // line
+            // 
+            this.line.Name = "line";
+            this.line.Size = new System.Drawing.Size(180, 22);
+            this.line.Text = "Line";
+            this.line.Click += new System.EventHandler(this.Line_Click);
+            // 
+            // fill
+            // 
+            this.fill.Name = "fill";
+            this.fill.Size = new System.Drawing.Size(180, 22);
+            this.fill.Text = "Fill";
+            this.fill.Click += new System.EventHandler(this.Fill_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 457);
+            this.Controls.Add(this.holst);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "OpenGL2D";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl holst;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem line;
+        private System.Windows.Forms.ToolStripMenuItem fill;
+    }
+}
+
---
+++ OpenGL2D/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Tao.FreeGlut;
+using Tao.OpenGl;
+using Tao.Platform;
+
+namespace OpenGL2D
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            holst.InitializeContexts();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Holst_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Line_Click(object sender, EventArgs e)
+        {
+            Gl.glViewport(0, 0, holst.Width, holst.Height);
+            Gl.glClearColor(0.0f, 0.0f, 0.250f, 1);
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Gl.glColor3f(0.392f, 0.584f, 0.929f);
+            Gl.glLineWidth(2);
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+                Gl.glVertex2f(-0.2f , 0);
+                Gl.glVertex2f(-0.6f,-0.4f);
+                Gl.glVertex2f(-0.4f,0.8f);
+                Gl.glVertex2f(0.4f,0.4f);
+                Gl.glVertex2f(-0.2f,-0.4f);
+            Gl.glEnd();
+            holst.Invalidate();
+        }
+
+        private void Fill_Click(object sender, EventArgs e)
+        {
+            Gl.glViewport(0, 0, holst.Width, holst.Height);
+            Gl.glClearColor(0.0f, 0.0f, 0.250f, 1);
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Gl.glBegin(Gl.GL_POLYGON);
+                Gl.glColor3f(0.274f, 0.509f, 0.705f);
+                Gl.glVertex2f(-0.2f, 0);
+                Gl.glColor3f(0.415f, 0.352f, 0.804f);
+                Gl.glVertex2f(-0.6f, -0.4f);
+                Gl.glColor3f(0.866f, 0.627f, 0.866f);
+                Gl.glVertex2f(-0.4f, 0.8f);
+                Gl.glColor3f(0.392f, 0.584f, 0.929f);
+                Gl.glVertex2f(0.4f, 0.4f);
+                Gl.glColor3f(0.254f, 0.411f, 0.882f);
+                Gl.glVertex2f(-0.2f, -0.4f);
+            Gl.glEnd();
+            holst.Invalidate();
+        }
+    }
+}
