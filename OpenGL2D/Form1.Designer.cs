23c9f879e2e8ce5e7c8bed0b8c1eac4aca1825e8
AntonyChatt <nyanchatgalaxy1988@gmail.com>
2020-04-26 00:28:26 +0700

    i kill u microsoftgit statusgit statuscd /c/Users/79234/Desktop/ST-front

--- OpenGL2D/Form1.Designer.cs
+++ OpenGL2D/Form1.Designer.cs
@@ -30,9 +30,13 @@
         {
             this.holst = new Tao.Platform.Windows.SimpleOpenGlControl();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.line = new System.Windows.Forms.ToolStripMenuItem();
-            this.fill = new System.Windows.Forms.ToolStripMenuItem();
+            this.typeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Line = new System.Windows.Forms.ToolStripMenuItem();
+            this.Fill = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.labelZero = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,55 +53,110 @@
             this.holst.BackColor = System.Drawing.Color.Black;
             this.holst.ColorBits = ((byte)(32));
             this.holst.DepthBits = ((byte)(16));
-            this.holst.Location = new System.Drawing.Point(-1, 27);
+            this.holst.Location = new System.Drawing.Point(-1, 33);
+            this.holst.Margin = new System.Windows.Forms.Padding(4);
             this.holst.Name = "holst";
-            this.holst.Size = new System.Drawing.Size(802, 535);
+            this.holst.Size = new System.Drawing.Size(913, 809);
             this.holst.StencilBits = ((byte)(0));
             this.holst.TabIndex = 0;
+            this.holst.Click += new System.EventHandler(this.Holst_Click);
             this.holst.Paint += new System.Windows.Forms.PaintEventHandler(this.Holst_Paint);
+            this.holst.KeyDown += new System.Windows.Forms.KeyEventHandler(this.holst_KeyDown);
+            this.holst.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Holst_MouseMove);
             // 
             // menuStrip1
             // 
-            this.menuStrip1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripMenuItem1});
+            this.typeToolStripMenuItem,
+            this.refreshToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(801, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(909, 28);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // toolStripMenuItem1
+            // typeToolStripMenuItem
             // 
-            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.line,
-            this.fill});
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(45, 20);
-            this.toolStripMenuItem1.Text = "Type";
+            this.typeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Line,
+            this.Fill});
+            this.typeToolStripMenuItem.Name = "typeToolStripMenuItem";
+            this.typeToolStripMenuItem.Size = new System.Drawing.Size(54, 24);
+            this.typeToolStripMenuItem.Text = "Type";
             // 
-            // line
+            // Line
             // 
-            this.line.Name = "line";
-            this.line.Size = new System.Drawing.Size(180, 22);
-            this.line.Text = "Line";
-            this.line.Click += new System.EventHandler(this.Line_Click);
+            this.Line.Name = "Line";
+            this.Line.Size = new System.Drawing.Size(119, 26);
+            this.Line.Text = "Line";
+            this.Line.Click += new System.EventHandler(this.Line_Click);
             // 
-            // fill
+            // Fill
             // 
-            this.fill.Name = "fill";
-            this.fill.Size = new System.Drawing.Size(180, 22);
-            this.fill.Text = "Fill";
-            this.fill.Click += new System.EventHandler(this.Fill_Click);
+            this.Fill.Name = "Fill";
+            this.Fill.Size = new System.Drawing.Size(119, 26);
+            this.Fill.Text = "Fill";
+            this.Fill.Click += new System.EventHandler(this.Fill_Click);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.BackColor = System.Drawing.SystemColors.WindowText;
+            this.labelX.Location = new System.Drawing.Point(279, 11);
+            this.labelX.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(17, 17);
+            this.labelX.TabIndex = 2;
+            this.labelX.Text = "X";
+            this.labelX.Visible = false;
+            this.labelX.Click += new System.EventHandler(this.LabelX_Click);
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.BackColor = System.Drawing.SystemColors.WindowText;
+            this.labelY.Location = new System.Drawing.Point(404, 11);
+            this.labelY.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(17, 17);
+            this.labelY.TabIndex = 3;
+            this.labelY.Text = "Y";
+            this.labelY.Visible = false;
+            // 
+            // labelZero
+            // 
+            this.labelZero.AutoSize = true;
+            this.labelZero.BackColor = System.Drawing.SystemColors.WindowText;
+            this.labelZero.Location = new System.Drawing.Point(535, 9);
+            this.labelZero.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelZero.Name = "labelZero";
+            this.labelZero.Size = new System.Drawing.Size(16, 17);
+            this.labelZero.TabIndex = 4;
+            this.labelZero.Text = "0";
+            this.labelZero.Visible = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(801, 457);
+            this.ClientSize = new System.Drawing.Size(909, 804);
+            this.Controls.Add(this.labelZero);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
             this.Controls.Add(this.holst);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(927, 851);
+            this.MinimumSize = new System.Drawing.Size(927, 851);
             this.Name = "Form1";
             this.Text = "OpenGL2D";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -112,9 +171,13 @@
 
         private Tao.Platform.Windows.SimpleOpenGlControl holst;
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem line;
-        private System.Windows.Forms.ToolStripMenuItem fill;
+        private System.Windows.Forms.ToolStripMenuItem typeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Line;
+        private System.Windows.Forms.ToolStripMenuItem Fill;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.Label labelZero;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
     }
 }
 
--- OpenGL2D/Form1.cs
+++ OpenGL2D/Form1.cs
@@ -16,6 +16,30 @@ namespace OpenGL2D
 {
     public partial class Form1 : Form
     {
+        float[,] ShapeA = new float[6, 4];
+        float[,] ShapeB = new float[4, 4];
+        float angleA = 0, angleB = 0;
+        float offsetBx = 0, offsetBy = 0, offsetAx = 0, offsetAy = 0;
+        float scaleB = 1, scaleA = 1;
+
+        void initializeShapes()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                ShapeA [i, 0] = 0.25f * (float)Math.Cos(Math.PI / 2 + 2 * Math.PI * i / 6);
+                ShapeA [i, 1] = 0.25f * (float)Math.Sin(Math.PI / 2 + 2 * Math.PI * i / 6);
+                ShapeA [i, 2] = 0;
+                ShapeA [i, 3] = 1;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                ShapeB [i, 0] = 0.25f * (float)Math.Cos(Math.PI / 4 + 2 * Math.PI * i / 4);
+                ShapeB [i, 1] = 0.25f * (float)Math.Sin(Math.PI / 4 + 2 * Math.PI * i / 4);
+                ShapeB [i, 2] = 0;
+                ShapeB [i, 3] = 1;
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -34,11 +58,15 @@ namespace OpenGL2D
 
         private void Line_Click(object sender, EventArgs e)
         {
+            float x0 = ClientSize.Width / 2;
+            float y0 = ClientSize.Height / 2;
+
+            drawAxises(x0, y0);
+
             Gl.glViewport(0, 0, holst.Width, holst.Height);
-            Gl.glClearColor(0.0f, 0.0f, 0.250f, 1);
-            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
             Gl.glColor3f(0.392f, 0.584f, 0.929f);
             Gl.glLineWidth(2);
+
             Gl.glBegin(Gl.GL_LINE_LOOP);
                 Gl.glVertex2f(-0.2f , 0);
                 Gl.glVertex2f(-0.6f,-0.4f);
@@ -46,14 +74,19 @@ namespace OpenGL2D
                 Gl.glVertex2f(0.4f,0.4f);
                 Gl.glVertex2f(-0.2f,-0.4f);
             Gl.glEnd();
+
             holst.Invalidate();
         }
 
         private void Fill_Click(object sender, EventArgs e)
         {
+            float x0 = ClientSize.Width / 2;
+            float y0 = ClientSize.Height / 2;
+
+            drawAxises(x0, y0);
+
             Gl.glViewport(0, 0, holst.Width, holst.Height);
-            Gl.glClearColor(0.0f, 0.0f, 0.250f, 1);
-            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+
             Gl.glBegin(Gl.GL_POLYGON);
                 Gl.glColor3f(0.274f, 0.509f, 0.705f);
                 Gl.glVertex2f(-0.2f, 0);
@@ -66,7 +99,359 @@ namespace OpenGL2D
                 Gl.glColor3f(0.254f, 0.411f, 0.882f);
                 Gl.glVertex2f(-0.2f, -0.4f);
             Gl.glEnd();
+
             holst.Invalidate();
+        }
+
+        void drawAxises (float x0, float y0)
+        {
+            Gl.glViewport(0, 0, holst.Width, holst.Height);
+
+            Gl.glLineWidth(3);
+            Gl.glColor3f(0.415f, 0.352f, 0.804f);
+
+            Gl.glBegin(Gl.GL_LINES);                
+                Gl.glVertex2f(1, 0);
+                Gl.glVertex2f(-1,0);
+            Gl.glEnd();
+
+            Gl.glBegin(Gl.GL_LINES);
+                Gl.glVertex2f(0, 1);
+                Gl.glVertex2f(0, -1);
+            Gl.glEnd();
+
+            Gl.glBegin(Gl.GL_TRIANGLES);
+            Gl.glVertex2f(0, 1);
+            Gl.glVertex2f(0.02f, 0.95f);
+            Gl.glVertex2f(-0.02f, 0.95f);
+            Gl.glEnd();
+
+            Gl.glBegin(Gl.GL_TRIANGLES);
+            Gl.glVertex2f(1, 0);
+            Gl.glVertex2f(0.95f, 0.02f);
+            Gl.glVertex2f(0.95f, -0.02f);
+            Gl.glEnd();
+
+            labelX.ForeColor = Color.SlateBlue;
+            labelY.ForeColor = Color.SlateBlue;
+            labelZero.ForeColor = Color.SlateBlue;
+
+            labelX.Font = new Font(this.Font, FontStyle.Bold);
+            labelY.Font = new Font(this.Font, FontStyle.Bold);
+            labelZero.Font = new Font(this.Font, FontStyle.Bold);
+
+            labelX.Location = new Point((int)(2*x0 - 15), (int)(y0 + 35));
+            labelY.Location = new Point((int)(x0 +15), 35);
+            labelZero.Location = new Point((int)(x0 + 35), (int)(y0+35));
+
+            labelX.Visible = true;
+            labelY.Visible = true;
+            labelZero.Visible = true;
+        }
+
+        void DrawA()
+        {
+            Gl.glViewport(0, 0, holst.Width, holst.Height);
+
+            Gl.glLineWidth(3);
+            
+                Gl.glBegin(Gl.GL_LINE_LOOP);
+                    Gl.glColor3f(0.274f, 0.509f, 0.705f);
+                    Gl.glVertex2f(ShapeA[0,0], ShapeA[0,1]);
+                    Gl.glColor3f(0.415f, 0.352f, 0.804f);
+                    Gl.glVertex2f(ShapeA[1,0], ShapeA[1,1]);
+                    Gl.glColor3f(0.866f, 0.627f, 0.866f);
+                    Gl.glVertex2f(ShapeA[2,0], ShapeA[2,1]);
+                    Gl.glColor3f(0.392f, 0.584f, 0.929f);
+                    Gl.glVertex2f(ShapeA[3,0], ShapeA[3,1]);
+                    Gl.glColor3f(0.254f, 0.411f, 0.882f);
+                    Gl.glVertex2f(ShapeA[4,0], ShapeA[4,1]);
+                    Gl.glColor3f(0.125f, 0.698f, 0.666f);
+                    Gl.glVertex2f(ShapeA[5,0], ShapeA[5,1]);
+                Gl.glEnd();
+
+            Gl.glEnable(Gl.GL_LINE_STIPPLE);
+            Gl.glLineStipple(2, 43690);
+            Gl.glLineWidth(2);
+
+                Gl.glBegin(Gl.GL_LINE_LOOP);
+                    Gl.glColor3f(0.274f, 0.509f, 0.705f);
+                    Gl.glVertex2f(ShapeA[0, 0], ShapeA[0, 1]);
+                    Gl.glColor3f(0.866f, 0.627f, 0.866f);
+                    Gl.glVertex2f(ShapeA[2, 0], ShapeA[2, 1]);
+                    Gl.glColor3f(0.254f, 0.411f, 0.882f);
+                    Gl.glVertex2f(ShapeA[4, 0], ShapeA[4, 1]);
+                Gl.glEnd();
+
+                Gl.glBegin(Gl.GL_LINE_LOOP);
+                    Gl.glColor3f(0.501f, 0, 0.501f);
+                    Gl.glVertex2f((ShapeA[0, 0] + ShapeA[2, 0]) / 2, (ShapeA[0, 1] + ShapeA[2, 1]) / 2);
+                    Gl.glColor3f(0.098f, 0.098f, 0.439f);
+                    Gl.glVertex2f((ShapeA[2, 0] + ShapeA[4, 0]) / 2, (ShapeA[2, 1] + ShapeA[4, 1]) / 2);
+                    Gl.glColor3f(0, 0.392f, 0);
+                    Gl.glVertex2f((ShapeA[0, 0] + ShapeA[4, 0]) / 2, (ShapeA[0, 1] + ShapeA[4, 1]) / 2);
+                Gl.glEnd();
+
+                Gl.glBegin(Gl.GL_LINES);
+                    Gl.glColor3f(0, 0, 0);
+                    Gl.glVertex2f((ShapeA[0, 0] + ShapeA[2, 0]) / 2, (ShapeA[0, 1] + ShapeA[2, 1]) / 2);
+                    Gl.glColor3f(0.415f, 0.352f, 0.804f);
+                    Gl.glVertex2f(ShapeA[1, 0], ShapeA[1, 1]);
+                Gl.glEnd();
+
+                Gl.glBegin(Gl.GL_LINES);
+                    Gl.glColor3f(0, 0, 0);
+                    Gl.glVertex2f((ShapeA[2, 0] + ShapeA[4, 0]) / 2, (ShapeA[2, 1] + ShapeA[4, 1]) / 2);
+                    Gl.glColor3f(0.392f, 0.584f, 0.929f);
+                    Gl.glVertex2f(ShapeA[3, 0], ShapeA[3, 1]);
+                Gl.glEnd();
+
+                Gl.glBegin(Gl.GL_LINES);
+                    Gl.glColor3f(0, 0, 0);
+                    Gl.glVertex2f((ShapeA[0, 0]+ ShapeA[4, 0]) / 2, (ShapeA[0, 1] + ShapeA[4, 1]) / 2);
+                    Gl.glColor3f(0.125f, 0.698f, 0.666f);
+                    Gl.glVertex2f(ShapeA[5, 0], ShapeA[5, 1]);
+                Gl.glEnd();
+
+            Gl.glDisable(Gl.GL_LINE_STIPPLE);
+        }
+
+        void rotateA()
+        {
+            Gl.glPushMatrix();
+            Gl.glRotatef(angleA, 0.0f, 0.0f, -1f);
+            Gl.glTranslatef(-0.5f, -0.5f, 0.0f);
+            Gl.glTranslatef(offsetAx, offsetAy, 0);
+            Gl.glScalef(scaleA, scaleA, 0);
+            DrawA();
+            Gl.glPopMatrix();
+            DrawB();
+            holst.Invalidate();
+        }
+
+        void singleTransition()
+        {
+            Gl.glPushMatrix();            
+            Gl.glTranslatef(-0.5f, -0.5f, 0.0f);
+            Gl.glRotatef(angleA, 0.0f, 0.0f, -1f);
+            Gl.glTranslatef(offsetAx, offsetAy, 0);
+            Gl.glScalef(scaleA, scaleA, 0);
+            DrawA();
+            Gl.glPopMatrix();
+
+            Gl.glPushMatrix();
+            Gl.glTranslatef(offsetBx, offsetBy, 0);
+            Gl.glRotatef(angleB, 0, 0, 1f);
+            Gl.glScalef(scaleB, scaleB, 0);
+            DrawB();
+            Gl.glPopMatrix();
+
+            holst.Invalidate();
+        }
+
+
+        void DrawB()
+        {
+            Gl.glViewport(0, 0, holst.Width, holst.Height);
+            Gl.glLineWidth(3);
+
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glColor3f(0.862f, 0.078f, 0.235f);
+            Gl.glVertex2f(ShapeB[0, 0], ShapeB[0, 1]);
+            Gl.glColor3f(0.804f, 0.360f, 0.360f);
+            Gl.glVertex2f(ShapeB[1, 0], ShapeB[1, 1]);
+            Gl.glColor3f(0.698f, 0.133f, 0.133f);
+            Gl.glVertex2f(ShapeB[2, 0], ShapeB[2, 1]);
+            Gl.glColor3f(0.545f, 0, 0);
+            Gl.glVertex2f(ShapeB[3, 0], ShapeB[3, 1]);
+            Gl.glEnd();
+
+              Gl.glBegin(Gl.GL_LINES);
+              Gl.glColor3f(0.603f, 0.803f, 0);
+              Gl.glVertex2f(ShapeB[0, 0], ShapeB[0, 1]);
+              Gl.glColor3f(0, 0.392f, 0);
+              Gl.glVertex2f(ShapeB[2, 0], ShapeB[2, 1]);
+              Gl.glEnd();
+
+              Gl.glBegin(Gl.GL_LINES);
+              Gl.glColor3f(0.603f, 0.803f, 0);
+              Gl.glVertex2f(ShapeB[1, 0], ShapeB[1, 1]);
+              Gl.glColor3f(0, 0.392f, 0);
+              Gl.glVertex2f(ShapeB[3, 0], ShapeB[3, 1]);
+              Gl.glEnd();
+        }
+        
+        private void holst_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Q:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    angleA += 5;
+                    rotateA();
+                    break;
+                case Keys.E:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    angleA -= 5;
+                    rotateA();
+                    break;
+
+                case Keys.G:
+                    for( int i = 0; i < 60; i++)
+                    {
+                        angleA += 5;
+                        rotateA();
+                    }
+                    break;
+                case Keys.T:
+                    for (int i = 0; i < 60; i++)
+                    {
+                        angleA -= 5;
+                        rotateA();
+                    }
+                    break;
+
+
+                case Keys.D:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    offsetAx += 0.05f;
+                    singleTransition();
+                    break;
+                case Keys.W:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    offsetAy += 0.05f;
+                    singleTransition();
+                    break;
+                case Keys.A:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    offsetAx -= 0.05f;
+                    singleTransition();
+                    break;
+                case Keys.S:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    offsetAy -= 0.05f;
+                    singleTransition();
+                    break;
+                case Keys.R:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    angleA += 5;
+                    singleTransition();
+                    break;
+                case Keys.F:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    angleA -= 5;
+                    singleTransition();
+                    break;
+                case Keys.Z:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    scaleA += 0.5f;
+                    singleTransition();
+                    break;
+                case Keys.X:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    scaleA -= 0.5f;
+                    singleTransition();
+                    break;
+
+                case Keys.L:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    offsetBx += 0.05f;
+                    singleTransition();
+                    break;
+                case Keys.I:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    offsetBy += 0.05f;
+                    singleTransition();
+                    break;
+                case Keys.J:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    offsetBx -= 0.05f;
+                    singleTransition();
+                    break;
+                case Keys.K:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    offsetBy -= 0.05f;
+                    singleTransition();
+                    break;
+                case Keys.O:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    angleB += 5;
+                    singleTransition();
+                    break;
+                case Keys.P:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    angleB -= 5;
+                    singleTransition();
+                    break;
+                case Keys.U:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    scaleB += 0.5f;
+                    singleTransition();
+                    break;
+                case Keys.H:
+                    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+                    scaleB -= 0.5f;
+                    singleTransition();
+                    break;
+
+                default:
+                    break;
+            }
+        }   
+        
+        void start()
+        {
+            Refresh();
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+
+            angleA = 0;
+            angleB = 0;
+            offsetBx = 0;
+            offsetBy = 0;
+            offsetAx = 0;
+            offsetAy = 0;
+            scaleB = 1;
+            scaleA = 1;
+
+            initializeShapes();
+
+            Gl.glPushMatrix();
+            Gl.glRotatef(angleA, 0.0f, 0.0f, -1f);
+            Gl.glTranslatef(-0.5f, -0.5f, 0.0f);
+            Gl.glTranslatef(offsetAx, offsetAy, 0);
+            Gl.glScalef(scaleA, scaleA, 0);
+            DrawA();
+            Gl.glPopMatrix();
+
+            Gl.glPushMatrix();
+            Gl.glTranslatef(offsetBx, offsetBy, 0);
+            Gl.glRotatef(angleB, 0, 0, 1f);
+            Gl.glScalef(scaleB, scaleB, 0);
+            DrawB();
+            Gl.glPopMatrix();
+
+            holst.Invalidate();
+        }
+
+        private void Holst_Click(object sender, EventArgs e)
+        {
+            //float x0 = ClientSize.Width / 2;
+            //float y0 = ClientSize.Height / 2;
+            //drawAxises(x0, y0);
+
+            start();
+        }       
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            start();
+        }       
+        
+        private void LabelX_Click(object sender, EventArgs e)
+        {
+        }
+        private void Holst_MouseMove(object sender, MouseEventArgs e)
+        {
         }
     }
 }
